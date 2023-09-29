2a07f12e433eb35e468f27776ce23fa3919d4721
Andreas Conrad <aco@seatable.io>
2023-09-30 01:33:40 +0200

    slight rework of the python test script

--- seatable-python-test.py
+++ seatable-python-test.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 #
-# This script writes a row in the "Name" column of the current table, reads it and writes it again.
+# This script prints the python version and all modules installed in the python runner container.
+# This script also writes a row in the "Name" column of the current table, reads it and writes it again.
+
 from seatable_api import Base, context
 from seatable_api.constants import ColumnTypes
+
+import sys
 
 def main():
     server_url = context.server_url or 'https://<your-seatable-server-url>'
@@ -11,10 +15,16 @@ def main():
     base = Base(api_token, server_url)
     base.auth()
 
+    print("Python version:", sys.version)
+    print("Modules:")
+    for module in sys.modules.keys():
+        if not module.startswith('_'):
+            print(module)
+
     print("Context ready, read/write test starting...")
 
     inital_rows_data = [{
-                'Name': 'This row is written by the python script.'
+                'Name': 'This row is written by the python script, read and written again.'
             }]
 
     base.batch_append_rows(context.current_table, inital_rows_data)
