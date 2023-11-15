c8157afc1524e30023f7dbecd1cd6dbdd00e3194
Christoph Dyllick-Brenzinger <cdb@datamate.org>
2023-11-15 17:27:17 +0100

    remove python3.6 from PYTHONPATH in scripts

--- scheduler/scripts/monitor.sh
+++ scheduler/scripts/monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PYTHONPATH=/opt/seatable-faas-scheduler/faas-scheduler:/usr/lib/python3.6/dist-packages:/usr/lib/python3.6/site-packages:/usr/local/lib/python3.6/dist-packages:/usr/local/lib/python3.6/site-packages
+export PYTHONPATH=/opt/seatable-faas-scheduler/faas-scheduler
 
 
 # log function
--- scheduler/src/faas-scheduler/clean_db.sh
+++ scheduler/src/faas-scheduler/clean_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PYTHONPATH=/opt/seatable-faas-scheduler/faas-scheduler:/usr/lib/python3.6/dist-packages:/usr/lib/python3.6/site-packages:/usr/local/lib/python3.6/dist-packages:/usr/local/lib/python3.6/site-packages
+export PYTHONPATH=/opt/seatable-faas-scheduler/faas-scheduler
 
 python3 /opt/seatable-faas-scheduler/faas-scheduler/faas_scheduler/clean_db.py  &>>/opt/seatable-faas-scheduler/logs/clean_db.log
