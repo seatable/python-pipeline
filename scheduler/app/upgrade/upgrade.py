8be02831169ed24b524fb9cbde3bac890197c3e6
AlexCXC <1223408988@qq.com>
2024-01-08 17:22:14 +0800

    4.2.7 4.2.11 -> 2.5.0 2.6.0

--- scheduler/app/upgrade/upgrade.py
+++ scheduler/app/upgrade/upgrade.py
@@ -22,7 +22,7 @@ DB_PASSWD = os.getenv('DB_PASSWD', '')
 DB_HOST = os.getenv('DB_HOST', 'seatable-mysql')
 DB_PORT = os.getenv('DB_PORT', '3306')
 DATABASE_NAME = os.getenv('DATABASE_NAME', 'scheduler')
-CURRENT_VERSION = os.getenv('VERSION', '4.2.1')
+CURRENT_VERSION = os.getenv('VERSION', '2.6.0')
 
 #SCHEDULER_VERSION_ENV = 'SCHEDULER_VERSION'
 top_dir = '/opt/scheduler/'
@@ -45,6 +45,8 @@ def collect_upgrade_scripts(from_version, to_version):
     We need to run 2.7.0.sql, 2.9.0.sql, and 3.0.0.sql.
     """
     scripts = []
+    from_version = tuple(int(v) for v in from_version.split('.'))
+    to_version = tuple(int(v) for v in to_version.split('.'))
     for fn in sorted(glob.glob(join(sql_dir, '*.*.*.sql'))):
         v = parse_upgrade_script_version(fn)
         if from_version < v and v <= to_version:
@@ -75,7 +77,7 @@ def read_version_stamp():
     try:
         last_one = db_session.execute(text(sql)).fetchone()
         if not last_one:  # version_history exists but no records
-            return '4.2.1'
+            return '2.2.4'
         return last_one[0]
     except Exception as e:
         return None
